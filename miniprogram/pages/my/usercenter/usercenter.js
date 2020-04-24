db1296db09d21ad5e3768aa29555576cb08233cc
tech11-much <18348792873@163.com>
2020-04-25 00:21:40 +0800

    登录功能已完成

--- miniprogram/pages/my/usercenter/usercenter.js
+++ miniprogram/pages/my/usercenter/usercenter.js
@@ -1,67 +1,86 @@
 // miniprogram/pages/my/usercenter/usercenter.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    userphoto: '/images/my/user-un.png',
-    userName: '杨河川',
+    userPhoto: "",
+    userName: "",
+    openid: "",
+    isHide: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function onLoad(options) {
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    var _this = this;
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {}
+    }).then((res) => {
+      //console.log(res);
+      db.collection('user').where({
+        _openid: res.result.openid
+      }).get().then((res) => {
+        if (res.data.length) {
+          app.userInfo = Object.assign(app.userInfo, res.data[0]);
+          console.log(app.userInfo);
+          console.log(res.data),
+            this.setData({
+              userPhoto: app.userInfo.userPhoto,
+              nickName: app.userInfo.nickName,
+              isHide: false
+            });
+        }
+        else {
+          _this.setData({
+            isHide : true
+          });
+        };
 
+      });
+
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  
+  getUserInfoClick: function getUserInfoClick(ev) {
+    var _this2 = this;
+    let userInfo = ev.detail.userInfo;
+    if (!this.data.logged && userInfo) {
+      db.collection('user').add({
+        data: {
+          userPhoto: userInfo.avatarUrl,
+          nickName: userInfo.nickName,
+          phoneNumber: '',
+          weixinNumber: '',
+          time: new Date(),
+          isLocation: true,
+          longitude: this.latitude,
+          latitude: this.latitude
+        }
+      }).then((res) => {
+        db.collection('user').doc(res._id).get().then((res) => {
+          app.userInfo = Object.assign(app.userInfo, res.data);
+          console.log(app.userInfo);
+          _this2.setData({
+            userPhoto: app.userInfo.userPhoto,
+            nickName: app.userInfo.nickName,
+            isHide: false
 
+          });
+        });
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })

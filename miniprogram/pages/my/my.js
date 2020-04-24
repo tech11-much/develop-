66810bafc0ed567b12d88ac152391d4ff608e965
BruceMao1 <1358915158@qq.com>
2020-04-24 20:20:35 +0800

    个人中心
    完善了未登陆的提醒，和已登录的界面，以及个人中心，还有个人信息的界面

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,11 +1,19 @@
 // miniprogram/pages/my/my.js
+const db = wx.cloud.database()
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userphoto: "/images/my/user-un.png",
+    userName: "Bruce Mao",
+    userAdress1: "ChengDu",
+    userAdress2: "China",
+    logged: true,
+    disabled: true,
   },
 
   /**
@@ -19,14 +27,43 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
+  //   wx.cloud.callFunction({
+  //     name: 'login',
+  //     data: {}
+  //   }).then((res) => {
+  //     // console.log(res);
+  //     db.collection('users').where({
+  //       _openid: res.result.openid,
+  //     }).get().then((res) => {
+  //       if (res.data.length) {
+  //         app.userInfo = Object.assign(app.userInfo, res.data[0])
+  //         this.setData({
+  //           userphoto: app.userInfo.userPhoto,
+  //           userName: app.userInfo.nickName,
+  //           userAdress1: app.userInfo.city,
+  //           userAdress2: app.userInfo.province,
+  //           logged: true,
+  //         })
+  //       }
+  //       else {
+  //         this.setData({
+  //           disabled: false,
+  //         })
+  //       }
+  //     })
+  //   })
+   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // this.setData({
+    //   userphoto: app.userInfo.userPhoto,
+    //   userName: app.userInfo.nickName,
+    //   userAdress1: app.userInfo.city,
+    //   userAdress2: app.userInfo.province,
+    // });
   },
 
   /**
@@ -62,5 +99,38 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  // bindGetUserInfo: function (ev) {
+  //   console.log(ev);
+  //   this.data.logged = true;
+  //   let userInfo = ev.detail.userInfo;
+  //   if (this.data.logged && userInfo) {
+  //     db.collection('users').add({
+  //       data: {
+  //         userPhoto: userInfo.avatarUrl,
+  //         nickName: userInfo.nickName,
+  //         province: userInfo.province,
+  //         city: userInfo.city,
+  //         time: new Date(),
+  //         phoneNumber: '',
+  //         islocation: false,
+  //         links: 1,
+  //       }
+  //     }).then((res) => {
+  //       db.collection('users').doc(res._id).get().then((res) => {
+  //         // console.log(res.data);
+  //         app.userInfo = Object.assign(app.userInfo, res.data);
+  //         this.setData({
+  //           userphoto: app.userInfo.userPhoto,
+  //           userName: app.userInfo.nickName,
+  //           userAdress1: app.userInfo.city,
+  //           userAdress2: app.userInfo.province,
+  //           logged: true,
+  //         })
+  //       })
+  //     });
+  //   }
+  // }
+
 })

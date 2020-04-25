10223f116a02ec0b8be560df1bd98bb1b22b6186
BruceMao1 <1358915158@qq.com>
2020-04-25 22:51:11 +0800

    发布界面
    发布界面要素

--- miniprogram/pages/up/up.js
+++ miniprogram/pages/up/up.js
@@ -5,7 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userphoto : '/images/up/addPic.png',
+    userName : '天天打游戏',
+    disabled : true,
+    array: ['学习', '娱乐', '生活'],
+    objectArray: [
+      {
+        id: 0,
+        name: '学习'
+      },
+      {
+        id: 1,
+        name: '娱乐'
+      },
+      {
+        id: 2,
+        name: '生活'
+      }
+    ],
+    index: 0,
   },
 
   /**
@@ -19,7 +37,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({
+      index : this.index,
+    });
   },
 
   /**
@@ -62,5 +82,33 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  handleUpdateImage() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths[0];
+        this.setData({
+          userphoto: tempFilePaths,
+          disabled: false,
+        });
+      }
+    })
+  },
+
 })

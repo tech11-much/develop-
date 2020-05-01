523a3c7c651ed5f32ae11cbbcdcf5cbe4ce0ad91
张征 <2042712521@qq.com>
2020-05-01 22:46:37 +0800

    首页末尾上拉刷新列表

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -2,91 +2,53 @@
 //const testImgUrl="https://7a7a-zz-11c835-1257008454.tcb.qcloud.la/gray.png";
 const db = wx.cloud.database()
 const app = getApp()
-const testImgUrl="https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg";
+const testImgUrl = "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg";
+const onePageNumber=10;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    background: [
-      {url:testImgUrl} ,  
-      {url:testImgUrl}
+    background: [{
+        url: testImgUrl
+      },{
+        url: testImgUrl
+      }
     ],
-    lists_study:[]
+    lists_study: [],
+    lists_show: [],
+    pageNumber: 10
   },
 
-  onSearch(){
+  onSearch() {
     console.log("search");
-    wx.navigateTo({ 
-      url: 'searchResult/searchResult?id=1', 
-      events: { 
-        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据 
-        acceptDataFromOpenedPage: function(data) { 
-          console.log(data) 
-        }, 
-        someEvent: function(data) { 
-          console.log(data) 
-        } 
-      }, 
-      success: function(res) { 
-        // 通过eventChannel向被打开页面传送数据 
-        res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' }) 
-      } 
-    });
-  },
-  toDetail(e){
-    // console.log(e);
-    // console.log(this);
-    console.log("toDetail");
-    let id=e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: './detail/detail?id='+id,
-    })
-  },
-  onCancel() {
-    console.log("search");
-  },
-  onClick() {
-    console.log("search");
-  },
-  changeIndicatorDots() {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-
-  changeAutoplay() {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-
-  intervalChange(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-
-  durationChange(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-      wx.navigateTo({
       url: 'searchResult/searchResult?id=1',
       events: {
-        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-        acceptDataFromOpenedPage: function(data) {
+        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据 
+        acceptDataFromOpenedPage: function (data) {
           console.log(data)
         },
-        someEvent: function(data) {
+        someEvent: function (data) {
           console.log(data)
         }
       },
-      success: function(res) {
-        // 通过eventChannel向被打开页面传送数据
-        res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' })
+      success: function (res) {
+        // 通过eventChannel向被打开页面传送数据 
+        res.eventChannel.emit('acceptDataFromOpenerPage', {
+          data: 'test'
+        })
       }
+    });
+  },
+  toDetail(e) {
+    // console.log(e);
+    // console.log(this);
+    console.log("toDetail");
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: './detail/detail?id=' + id,
     })
   },
 
@@ -95,6 +57,7 @@ Page({
    */
   onLoad: function (options) {
     this.getListData();
+    
   },
 
   /**
@@ -129,29 +92,38 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('refresh');
+    this.setData({
+      lists_study: [],
+      lists_show:[]
+    });
+    this.getListData();
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.getNewPage();
   },
-  test(){
+  test() {
     console.log("test");
-    wx.startPullDownRefresh();
-    wx.stopPullDownRefresh();
-  },
-  onPullDownRefresh(){
-    console.log('refresh');
+    // wx.startPullDownRefresh();
+    // wx.stopPullDownRefresh();
     this.setData({
-      lists_study:[]
+      // lists_show: this.data.lists_show + this.data.lists_study.slice(this.data.pageNumber, this.data.pageNumber + 10),
+      lists_show:this.data.lists_study.slice(0, this.data.pageNumber),
+      pageNumber:this.data.pageNumber+onePageNumber
     });
-    this.getListData();
-    wx.stopPullDownRefresh();
   },
-
+  getNewPage() {
+    console.log("上拉刷新获取更多列表");
+    this.setData({
+      lists_show:this.data.lists_study.slice(0, this.data.pageNumber),
+      pageNumber:this.data.pageNumber+onePageNumber
+    });
+  },
   /**
    * 用户点击右上角分享
    */
@@ -160,16 +132,17 @@ Page({
   },
   getListData() {
     wx.cloud.callFunction({
-      // 云函数名称
-      name: 'getData',
-    })
+        // 云函数名称
+        name: 'getData',
+      })
       .then(res => {
         console.log(res);
         this.setData({
-          lists_study: res.result.data
+          lists_study: res.result.data,
+          lists_show:res.result.data.slice(0,onePageNumber-1)
         });
       })
-      .catch(console.error)
-    
+      .catch(console.error);
+    console.log("刷新列表");
   },
 })

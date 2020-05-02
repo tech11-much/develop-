1dbff6cdfdbf78c5e446c60c2f03daa486cf3af6
tech11-much <18348792873@163.com>
2020-05-02 18:34:09 +0800

    ddddd

--- miniprogram/pages/myPublication/myPublication.js
+++ miniprogram/pages/myPublication/myPublication.js
@@ -1,17 +1,21 @@
 // miniprogram/pages/myPublication/myPublication.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name : '',
+    goodsMine : []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+   
 
   },
 
@@ -19,7 +23,24 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {}
+    }).then((res) => {
+      //console.log(res);
+      db.collection('goods').where({
+        _openid: res.result.openid
+      }).field({
+        productName: true,
+        _id: true,
+        productPrice: true
+      }).get().then((res) => {
+        console.log(res)
+        this.setData({
+          goodsMine: res.data
+        });
+      });
+    });
   },
 
   /**
@@ -61,6 +82,28 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  deleteGoods : function(e){
+    console.log(e);
+    let id = e.target.id
+    db.collection('goods').where({
+      _id : id
+    }).remove().then(res => {
+      wx.showToast({
+        title: '已成功删除信息',
+        icon: '',
+        image: '',
+        duration: 1000,
+        mask: true,
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+      
+    })
+      .catch(console.error)
+
 
   }
 })
